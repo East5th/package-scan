55d9ad3c620f609aff1530008fbb713a5f3733c8
Pete Corey <petecorey@gmail.com>
2015-04-24 13:46:51 -0700

    Refactors alerts schema & changed name to east5th

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'pcorey:package-scan',
+  name: 'east5th:package-scan',
   version: '0.0.1',
   summary: 'Looks through your installed package list looking for dangerous packages.',
   git: 'https://github.com/East5th/package-scan',
