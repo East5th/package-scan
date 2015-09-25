8120eeaed88b77bbab8d573544b0e1a4c1c4f8ff
Pete Corey <petecorey@gmail.com>
2015-09-24 18:35:28 -0700

    Added links support, and switched to addAssets

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'east5th:package-scan',
-    version: '0.0.1',
+    version: '0.0.2',
     summary: 'Looks through your installed package list looking for dangerous packages.',
     git: 'https://github.com/East5th/package-scan',
     documentation: 'README.md',
@@ -8,10 +8,10 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.1.0.2');
+    api.versionsFrom('1.2.0.1');
     api.use('http');
     api.addFiles('lib/package-scan.js', 'server');
-    api.addFiles('data/alerts.json', 'server', {isAsset: true});
+    api.addAssets('data/alerts.json', 'server');
 });
 
 Npm.depends({
